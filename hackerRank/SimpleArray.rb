d6d7c68674be6f14413d9f04ce7ae5896b55b338
Jonathan <jonitoadriano@gmail.com>
2023-03-18 23:26:23 -0500

    Update SimpleArray.rb

--- hackerRank/SimpleArray.rb
+++ hackerRank/SimpleArray.rb
@@ -1,1 +1,27 @@
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar)
+    sum = 0
+    ar.each do |num|
+       sum = sum + num 
+    end
+return sum
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+ar_count = gets.strip.to_i
+
+ar = gets.rstrip.split.map(&:to_i)
+
+result = simpleArraySum ar
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
 

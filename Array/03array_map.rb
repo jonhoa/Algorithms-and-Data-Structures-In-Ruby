65000cd88285064fdd3eee46c7464734b5d7de36
Jonathan <jonitoadriano@gmail.com>
2023-03-19 23:06:04 -0500

    Create 03array_map.rb

---
+++ Array/03array_map.rb
@@ -0,0 +1,11 @@
+#  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
+#     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+array_hashes = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+array = []
+
+array_hashes.each do |value|
+  p value[:name]
+  array << value[:name]
+end
+
+p array

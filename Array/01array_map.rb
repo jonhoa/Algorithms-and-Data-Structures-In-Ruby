49b74f18b1a1ac6ed03f8b9cc4618b49d1adc24d
Jonathan <jonitoadriano@gmail.com>
2023-03-19 22:42:21 -0500

    Update 01array_map.rb

--- Array/01array_map.rb
+++ Array/01array_map.rb
@@ -1,1 +1,9 @@
+#  1. Start with an array of numbers and create a new array with each number times 3.
+#     For example, [1, 2, 3] becomes [3, 6, 9]
+arry = [1,2,3]
+new_arry = []
 
+arry.each do |num|
+  new_arry << num*3
+end
+p new_arry

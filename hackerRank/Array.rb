394536db3f1af899c691c3304e4a2b6b7892a0c0
Jonathan <jonitoadriano@gmail.com>
2023-04-01 22:55:42 -0500

    Update Array.rb

--- hackerRank/Array.rb
+++ hackerRank/Array.rb
@@ -1,1 +1,8 @@
+# Count the number of times a number is repeated in an array
+arr = [ 1, 2, 3, 5, 6, 7, 7, 7, 3, 3, 4, 1, 1, 1, 5, 3]
+counts = Hash.new(0) #Empty hash
 
+arr.each do |num|
+   counts[num]+= 1
+end
+p counts
